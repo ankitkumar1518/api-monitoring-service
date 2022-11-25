103f616b9df9923c23345f6e7dff6fed0fbeeb41
ankitkumar1518 <ankitkumar@quantela.com>
2022-11-25 18:51:08 +0530

    added pino logger

--- logger.js
+++ logger.js
@@ -1,6 +1,45 @@
 const { createLogger, format, transports, config } = require('winston');
 const pino = require('pino');
+const moment = require("moment");
 
+
+const levels = {
+    fatal: 0,
+    error: 1,
+    warn: 2,
+    info: 3,
+    verbose: 4,
+    debug: 5,
+    silly: 6
+}
+
+const options = {
+    mixin() {
+        return {
+            "application": "data-pipeline",
+            "timestamp": moment().toISOString()
+        }
+    },
+    "customLevels": levels,
+    "useOnlyCustomLevels": true,
+    "messageKey": "message",
+    "timestamp": false,
+    "level": "fatal",
+    "formatters": {
+        level(label, number) {
+            return { "level": label }
+        },
+        log(object) {
+            return object
+        }
+    }
+}
+
+const stream = pino.destination({
+    dest: './logs/api-monitoring.log',
+    minLength: 1024, //4096 Buffer before writing
+    sync: false, // Asynchronous logging
+})
 
 
 const logger = createLogger({
@@ -10,4 +49,4 @@ const logger = createLogger({
     ]
 });
 // module.exports = logger;
-module.exports = pino({});
+module.exports = pino(options, stream);
